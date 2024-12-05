a99931f8936afc843539501ff3ddf1d1a0d26834
AdrianIchiro <adrianfahren10@gmail.com>
2024-12-05 22:29:57 +0700

    lanjutan dari landingPage

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
 </head>
+
 <body>
     @inertia
+    <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            duration: 5000,
+            easing: 'ease-in-out',
+            once: true
+        });
+    </script>
+
 </body>
+
+
 </html>
