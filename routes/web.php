04b7f6f0490bd7ec51eb991733e5b53a78c0e118
AdrianIchiro <adrianfahren10@gmail.com>
2024-12-30 23:49:04 +0700

    membuat order

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AuthMiddleware;
 
 
-<<<<<<< HEAD
+
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login.show');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/check-login', [AuthController::class, 'checkLogin']);
@@ -16,14 +16,6 @@ Route::get('/check-login', [AuthController::class, 'checkLogin']);
 Route::get('/product/{id}', [AuthController::class, 'showProductDetail'])->name('product.detail');
 Route::get('/', [AuthController::class, 'showHome'])->name('home.show');
 Route::post('/addToCart', [AuthController::class, 'addToCart'])->name('addToCart')->middleware(AuthMiddleware::class);
-=======
-
-
-
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login.show');//kevin
-Route::post('/login', [AuthController::class, 'login'])->name('login');//kevin
-Route::get('/check-login', [AuthController::class, 'checkLogin']);//gk kepake
->>>>>>> 036765715659770753e8f5a233f156fb888ac46d
 
 Route::get('/product/{id}', [AuthController::class, 'showProductDetail'])->name('product.detail');//kevin
 Route::get('/', [AuthController::class, 'showHome'])->name('home.show');//kevin//adrian
