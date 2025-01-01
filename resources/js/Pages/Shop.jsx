8abd41ffeb40451c14ed924fc45c8590c1c644ef
1shtalica <adibfaizulhaq05@gmail.com>
2025-01-01 16:07:31 +0700

    sub, specific, search, clear

--- resources/js/Pages/Shop.jsx
+++ resources/js/Pages/Shop.jsx
@@ -6,6 +6,8 @@ import ShopListOfProducts from "../component/Shop_List_Products";
 import FooterLanding from "../component/FooterSection";
 
 import CategoryModal from "../component/Shop_Category_Modal";
+import SubCategoryModal from "../component/Shop_SubCategory_Modal";
+import SpecificCategoryModal from "../component/Shop_SpecificCategory_Modal";
 
 import { Inertia } from "@inertiajs/inertia";
 import { Link } from "@inertiajs/react";
@@ -58,13 +60,68 @@ const ShopPage = ({
         // Inertia.get(`/shop?${params.toString()}`);
     };
 
+    //filter kategori
     useEffect(() => {
         if (selectedCategoryFilter != null) {
             router.visit(`/shop?categoryId=${selectedCategoryFilter}`, {
                 method: "get",
             });
         }
-    }, []);
+    }, [selectedCategoryFilter]);
+
+    //filter subkategori
+    useEffect(() => {
+        if (selectedSubCategoryFilter != null) {
+            router.visit(`/shop?subcategoryId=${selectedSubCategoryFilter}`, {
+                method: "get",
+            });
+        }
+    }, [selectedSubCategoryFilter]);
+
+    //filter specifickategori
+    useEffect(() => {
+        if (selectedSpecificCategoryFilter != null) {
+            router.visit(
+                `/shop?specificSubcategoryId=${selectedSpecificCategoryFilter}`,
+                {
+                    method: "get",
+                }
+            );
+        }
+    }, [selectedSpecificCategoryFilter]);
+
+    //search
+    //ganti value search
+    const handleSearchChange = (e) => {
+        const value = e.target.value;
+        setSearchValue(value);
+        handleSearch(value);
+    };
+
+    //lakukan pencarian
+    const handleSearch = (value) => {
+        if (value.trim() === "") {
+            return;
+        }
+
+        router.visit(`/shop?search=${encodeURIComponent(value)}`, {
+            method: "get",
+        });
+    };
+
+    //Clear
+    const handleReset = () => {
+        // Reset semua filter
+        setSelectedCategory(null);
+        setSelectedSubCategory(null);
+        setSelectedSpecificCategory(null);
+        setSearchValue("");
+
+        // Kunjungi halaman shop tanpa filter
+        router.visit("/shop", {
+            method: "get",
+        });
+    };
 
     return (
         <div>
@@ -80,11 +137,23 @@ const ShopPage = ({
                     >
                         Category
                     </button>
-                    <button className="hover:bg-NusantaraGoldDark text-white px-4 py-2 rounded">
+                    <button
+                        className="hover:bg-NusantaraGoldDark text-white px-4 py-2 rounded"
+                        onClick={() => setShowSubCategoryModal(true)}
+                    >
                         SubCategory
                     </button>
-                    <button className="hover:bg-NusantaraGoldDark text-white px-4 py-2 rounded">
+                    <button
+                        className="hover:bg-NusantaraGoldDark text-white px-4 py-2 rounded"
+                        onClick={() => setShowSpecificCategoryModal(true)}
+                    >
                         Specific
+                    </button>
+                    <button
+                        className="hover:bg-NusantaraGoldDark text-white px-4 py-2 rounded"
+                        onClick={handleReset}
+                    >
+                        Clear
                     </button>
                 </div>
                 <div className="sm:hidden relative">
@@ -96,14 +165,31 @@ const ShopPage = ({
                     </button>
                     {isDropdownOpen && (
                         <div className="absolute top-full left-0 w-full bg-NusantaraGold hover:bg-NusantaraGold-400 text-white shadow-lg">
-                            <button className="block w-full px-4 py-2 text-left hover:bg-NusantaraGoldDark">
+                            <button
+                                className="block w-full px-4 py-2 text-left hover:bg-NusantaraGoldDark"
+                                onClick={() => setShowCategoryModal(true)}
+                            >
                                 Category
                             </button>
-                            <button className="block w-full px-4 py-2 text-left hover:bg-NusantaraGoldDark">
+                            <button
+                                className="block w-full px-4 py-2 text-left hover:bg-NusantaraGoldDark"
+                                onClick={() => setShowSubCategoryModal(true)}
+                            >
                                 SubCategory
                             </button>
-                            <button className="block w-full px-4 py-2 text-left hover:bg-NusantaraGoldDark">
+                            <button
+                                className="block w-full px-4 py-2 text-left hover:bg-NusantaraGoldDark"
+                                onClick={() =>
+                                    setShowSpecificCategoryModal(true)
+                                }
+                            >
                                 Specific
+                            </button>
+                            <button
+                                className="block w-full px-4 py-2 text-left hover:bg-NusantaraGoldDark"
+                                onClick={handleReset()}
+                            >
+                                Clear
                             </button>
                         </div>
                     )}
@@ -115,7 +201,7 @@ const ShopPage = ({
                         className="justify-end w-full sm:w-1/2 h-10 bg-white p-2 rounded resize-none focus:outline-none focus:ring-2 focus:ring-NusantaraGoldLight overflow-hidden"
                         placeholder="Search products..."
                         value={searchValue}
-                        onChange={(e) => setSearchValue(e.target.value)}
+                        onChange={handleSearchChange}
                     />
                 </div>
             </div>
@@ -135,6 +221,22 @@ const ShopPage = ({
                 categoriesData={categories}
                 apply={handleFilterChange}
                 setSelectedCategory={setSelectedCategory}
+            />
+
+            <SubCategoryModal
+                isVisible={showSubCategoryModal}
+                onClose={() => setShowSubCategoryModal(false)}
+                subCategoriesData={subCategories}
+                apply={handleFilterChange}
+                setSelectedSubCategory={setSelectedSubCategory}
+            />
+
+            <SpecificCategoryModal
+                isVisible={showSpecificCategoryModal}
+                onClose={() => setShowSpecificCategoryModal(false)}
+                specificCategoriesData={specificCategories}
+                apply={handleFilterChange}
+                setSelectedSpecificCategory={setSelectedSpecificCategory}
             />
         </div>
     );
